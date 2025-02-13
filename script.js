62ba13b391d0dbc25ada52061fec78f2dc282783
unknown <mmacpin@g.educaand.es>
2025-02-13 16:38:30 +0100

    III Subida

--- script.js
+++ script.js
@@ -16,4 +16,80 @@ document.addEventListener('DOMContentLoaded', function() {
       localStorage.setItem('theme', 'light');
     }
   });
+
+  // // Cargar palabras y seleccionar una aleatoria
+  // fetch('palabra.json')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     const palabras = data.palabras;
+  //     iniciarJuego(palabras);
+  //   });
+
+  // function iniciarJuego(palabras) {
+  //   const palabraSecreta = palabras[Math.floor(Math.random() * palabras.length)];
+  //   let intentos = 0;
+  //   const maxIntentos = 5;
+  //   const wordleContainer = document.getElementById('wordle-container');
+  //   wordleContainer.innerHTML = '';
+
+  //   for (let i = 0; i < maxIntentos; i++) {
+  //     for (let j = 0; j < palabraSecreta.length; j++) {
+  //       const cell = document.createElement('div');
+  //       cell.classList.add('cell');
+  //       cell.setAttribute('contenteditable', 'true');
+  //       cell.setAttribute('data-row', i);
+  //       cell.setAttribute('data-col', j);
+  //       wordleContainer.appendChild(cell);
+  //     }
+  //   }
+
+  //   document.addEventListener('keydown', function(event) {
+  //     if (event.key === 'Enter') {
+  //       validarIntento(palabraSecreta, intentos);
+  //       intentos++;
+  //       if (intentos >= maxIntentos) {
+  //         alert('Has agotado tus intentos. La palabra era: ' + palabraSecreta);
+  //         reiniciarJuego();
+  //       }
+  //     }
+  //   });
+  // }
+
+  // function validarIntento(palabraSecreta, intento) {
+  //   const wordleContainer = document.getElementById('wordle-container');
+  //   const cells = wordleContainer.querySelectorAll(`[data-row="${intento}"]`);
+  //   let intentoPalabra = '';
+
+  //   cells.forEach(cell => {
+  //     intentoPalabra += cell.textContent.toLowerCase();
+  //   });
+
+  //   if (intentoPalabra.length !== palabraSecreta.length) {
+  //     alert('Completa todas las letras antes de validar.');
+  //     return;
+  //   }
+
+  //   cells.forEach((cell, index) => {
+  //     const letra = cell.textContent.toLowerCase();
+  //     if (letra === palabraSecreta[index]) {
+  //       cell.classList.add('correct');
+  //     } else if (palabraSecreta.includes(letra)) {
+  //       cell.classList.add('present');
+  //     } else {
+  //       cell.classList.add('absent');
+  //     }
+  //   });
+
+  //   if (intentoPalabra === palabraSecreta) {
+  //     alert('¡Felicidades! Has adivinado la palabra.');
+  //     reiniciarJuego();
+  //   }
+  // }
+
+   window.reiniciarJuego = function() {
+    location.reload();
+}});
+let refresh = document.getElementsByClassName('refresh');
+refresh.addEventListener('click', _ => {
+            location.reload();
 });
